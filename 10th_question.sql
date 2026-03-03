c32a9b739699c1a88fe300f8c9f7ef11302ac4ac
venkata sai prasad muddada <s.muddada@ajacs.in>
2026-03-03 18:26:01 +0530

    sql practice questions

---
+++ 10th_question.sql
@@ -0,0 +1,31 @@
+-- Companies often perform salary analyses to ensure fair compensation practices. One useful analysis is to check if there are any employees earning more than their direct managers.
+
+-- As a HR Analyst, you're asked to identify all employees who earn more than their direct managers. The result should include the employee's ID and name.
+
+-- employee Schema:
+-- column_name	type	description
+-- employee_id	integer	The unique ID of the employee.
+-- name	string	The name of the employee.
+-- salary	integer	The salary of the employee.
+-- department_id	integer	The department ID of the employee.
+-- manager_id	integer	The manager ID of the employee.
+-- employee Example Input:
+-- employee_id	name	salary	department_id	manager_id
+-- 1	Emma Thompson	3800	1	6
+-- 2	Daniel Rodriguez	2230	1	7
+-- 3	Olivia Smith	7000	1	8
+-- 4	Noah Johnson	6800	2	9
+-- 5	Sophia Martinez	1750	1	11
+-- 6	Liam Brown	13000	3	NULL
+-- 7	Ava Garcia	12500	3	NULL
+-- 8	William Davis	6800	2	NULL
+-- Example Output:
+-- employee_id	employee_name
+-- 3	Olivia Smith
+-- The output shows that Olivia Smith earns $7,000, surpassing her manager, William David who earns $6,800.
+
+SELECT e1.employee_id, e1.name as employee_name
+from employee e1
+join employee e2
+on e1.manager_id = e2.employee_id
+where e1.salary>e2.salary
---
+++ 11th_question.sql
@@ -0,0 +1,29 @@
+-- Given a table containing information about bank deposits and withdrawals made using Paypal, write a query to retrieve the final account balance for each account, taking into account all the transactions recorded in the table with the assumption that there are no missing transactions.
+
+-- transactions Table:
+-- Column Name	Type
+-- transaction_id	integer
+-- account_id	integer
+-- amount	decimal
+-- transaction_type	varchar
+-- transactions Example Input:
+-- transaction_id	account_id	amount	transaction_type
+-- 123	101	10.00	Deposit
+-- 124	101	20.00	Deposit
+-- 125	101	5.00	Withdrawal
+-- 126	201	20.00	Deposit
+-- 128	201	10.00	Withdrawal
+-- Example Output:
+-- account_id	final_balance
+-- 101	25.00
+-- 201	10.00
+-- Using account ID 101 as an example, $30.00 was deposited into this account, while $5.00 was withdrawn. Therefore, the final account balance can be calculated as the difference between the total deposits and withdrawals which is $30.00 - $5.00, resulting in a final balance of $25.00.
+select 
+account_id,
+sum(
+case
+when transaction_type='Deposit' then amount
+when transaction_type='Withdrawal' then -amount
+END )as final_balance
+from transactions
+group by account_id
